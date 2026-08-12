6d7e8d45f646a2281fbb495422f58c0a34dfc03f
michalsurmanek-sketch <230581147+michalsurmanek-sketch@users.noreply.github.com>
2026-08-12 17:19:45 +0200

    Use cacheable homepage leaflet controller

--- assets/home-all-stores.js
+++ assets/home-all-stores.js
@@ -5,7 +5,7 @@
 
   if (!document.querySelector('script[data-slevao-leaflet-control]')) {
     const control = document.createElement('script');
-    control.src = `assets/home-leaflet-control.js?v=20260802-1-${Date.now()}`;
+    control.src = `assets/home-leaflet-control.js?v=20260812-2`;
     control.defer = true;
     control.dataset.slevaoLeafletControl = 'true';
     document.head.append(control);
