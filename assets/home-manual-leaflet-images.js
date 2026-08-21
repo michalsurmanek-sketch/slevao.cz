0c1692ce75c2d46d8e058b4c13f2c2ab18636451
michalsurmanek-sketch <230581147+michalsurmanek-sketch@users.noreply.github.com>
2026-08-21 18:19:30 +0200

    Align manual leaflet image loader with current control build

--- assets/home-manual-leaflet-images.js
+++ assets/home-manual-leaflet-images.js
@@ -2,7 +2,7 @@
   'use strict';
   if (document.querySelector('script[data-slevao-leaflet-control]')) return;
   const script = document.createElement('script');
-  script.src = `assets/home-leaflet-control.js?v=20260812-2`;
+  script.src = `assets/home-leaflet-control.js?v=20260821-1`;
   script.defer = true;
   script.dataset.slevaoLeafletControl = 'true';
   document.head.append(script);
