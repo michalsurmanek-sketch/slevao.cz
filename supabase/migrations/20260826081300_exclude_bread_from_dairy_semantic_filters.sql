76c6379e9f25576c0ac2859dd0e658d29e576ef3
michalsurmanek-sketch <230581147+michalsurmanek-sketch@users.noreply.github.com>
2026-08-26 10:20:36 +0200

    Keep bakery items out of dairy quick filters

---
+++ supabase/migrations/20260826081300_exclude_bread_from_dairy_semantic_filters.sql
@@ -0,0 +1,96 @@
+create or replace function public.public_offer_semantic_tags(p_search text)
+returns text[]
+language plpgsql
+immutable parallel safe
+set search_path to 'public','pg_temp'
+as $function$
+declare
+  s text := public.normalize_text(coalesce(p_search,''));
+  tags text[] := '{}'::text[];
+  beer boolean;
+  milk boolean;
+  bread boolean;
+  eggs boolean;
+  butter boolean;
+  cheese boolean;
+  meat boolean;
+  chicken boolean;
+  prsa_meat boolean;
+  pet_context boolean;
+  meat_alternative boolean;
+  fruit_any boolean;
+  fruit boolean;
+  veg boolean;
+begin
+  beer := s ~ '\m(pivo|pivni|lezak|radler|pils[a-z0-9]*|porter|stout)\M';
+  milk := s ~ '\m(mleko|mlecny|mlecne|plnotuc[a-z0-9]*|polotuc[a-z0-9]*|odtuc[a-z0-9]*|bezlaktoz[a-z0-9]*|kondenzovan[a-z0-9]*)\M';
+  bread := s ~ '\m(peciv[a-z0-9]*|chleb[a-z0-9]*|rohlik[a-z0-9]*|housk[a-z0-9]*|baget[a-z0-9]*|veka|dalam[a-z0-9]*|kaiser[a-z0-9]*|toast[a-z0-9]*|koblih[a-z0-9]*|croissant[a-z0-9]*|kolac[a-z0-9]*|buchta|zavin[a-z0-9]*|snek)\M'
+    and s !~ '\mzavinace\M';
+  eggs := s ~ '\m(vejce|vajec[a-z0-9]*)\M';
+  butter := s ~ '\m(maslo|maslicko|ghee|ghi)\M';
+  cheese := s ~ '\m(syr[a-z0-9]*|eidam[a-z0-9]*|gouda|emental[a-z0-9]*|hermelin[a-z0-9]*|niva|mozzarell[a-z0-9]*|cheddar[a-z0-9]*|parenic[a-z0-9]*|korbac[a-z0-9]*|balkansk[a-z0-9]*|cottage)\M';
+
+  -- Rychlé filtry popisují, co výrobek je, ne pouze ingredienci v názvu.
+  -- Pečivo se sýrem/máslem/mlékem/vejcem proto zůstává pečivem.
+  if bread then
+    milk := false;
+    eggs := false;
+    butter := false;
+    cheese := false;
+  end if;
+
+  prsa_meat := s ~ '\mprsa\M'
+    and s !~ '\m(paska|podprsenk[a-z0-9]*|nalepk[a-z0-9]*|ochran[a-z0-9]*|bradavk[a-z0-9]*|kosmetik[a-z0-9]*|odev[a-z0-9]*|trick[a-z0-9]*|top|plavk[a-z0-9]*)\M';
+
+  pet_context := s ~ '\m(akinu|dingo|dog snaq|prevital|vetamix|vitakraft|shinycat|huhubamboo|propesko|felix fantastic|whiskas|pedigree|purina)\M'
+    or s ~ '\m(pro psy|pro kocky|pro psa|granule|pamlsk[a-z0-9]*|krmivo|kapsicky pro)\M';
+  meat_alternative := s ~ '\m(alternativ[a-z0-9]*|vegetari[a-z0-9]*|vegansk[a-z0-9]*|rostlinn[a-z0-9]*|veggie)\M'
+    and s ~ '\mmasov[a-z0-9]*\M';
+
+  meat := s ~ '\m(maso|masov[a-z0-9]*|vepr[a-z0-9]*|hovez[a-z0-9]*|kure[a-z0-9]*|kurec[a-z0-9]*|kruti|kruta|kruty|kachn[a-z0-9]*|jehnec[a-z0-9]*|kralic[a-z0-9]*|krkovic[a-z0-9]*|kyta|plec|kotlet[a-z0-9]*|panenk[a-z0-9]*|svickov[a-z0-9]*|rosten[a-z0-9]*|steak[a-z0-9]*|zebro|zeber|bucek|koleno|gulasov[a-z0-9]*|jatr[a-z0-9]*)\M'
+    or prsa_meat;
+  if pet_context or meat_alternative then meat := false; end if;
+
+  chicken := s ~ '\m(kure[a-z0-9]*|kurec[a-z0-9]*)\M'
+    or prsa_meat;
+  if pet_context or meat_alternative then chicken := false; end if;
+
+  fruit_any := s ~ '\m(jablk[a-z0-9]*|hrusk[a-z0-9]*|banan[a-z0-9]*|pomeranc[a-z0-9]*|mandarink[a-z0-9]*|citron[a-z0-9]*|limet[a-z0-9]*|grep[a-z0-9]*|hrozn[a-z0-9]*|jahod[a-z0-9]*|malin[a-z0-9]*|boruv[a-z0-9]*|ostruzin[a-z0-9]*|rybiz[a-z0-9]*|tresn[a-z0-9]*|visn[a-z0-9]*|merunk[a-z0-9]*|broskv[a-z0-9]*|nektarink[a-z0-9]*|svestk[a-z0-9]*|mango|ananas[a-z0-9]*|avokad[a-z0-9]*|kiwi|meloun[a-z0-9]*|maraku[a-z0-9]*|papaj[a-z0-9]*)\M';
+  fruit := fruit_any
+    and s !~ '\m(napoj[a-z0-9]*|dzus[a-z0-9]*|juice|nektar|stava|smoothie|sirup[a-z0-9]*|syrob[a-z0-9]*|liker[a-z0-9]*|rum|vodka|gin|vino|pivo|cider|jogurt[a-z0-9]*|dezert[a-z0-9]*|tycink[a-z0-9]*|bonbon[a-z0-9]*|cokolad[a-z0-9]*|zmrzlin[a-z0-9]*|dzem[a-z0-9]*|marmelad[a-z0-9]*|kompot[a-z0-9]*|pyre|caj|susen[a-z0-9]*|lyo[a-z0-9]*|lyofiliz[a-z0-9]*|mrazen[a-z0-9]*|kandovan[a-z0-9]*|ovocn[a-z0-9]*|prichut[a-z0-9]*|zubni|pasta|tablet[a-z0-9]*|vitamin[a-z0-9]*|gummies|rasenk[a-z0-9]*|kosmetik[a-z0-9]*|prostred[a-z0-9]*|cistic[a-z0-9]*|mydlo|sampon[a-z0-9]*|aroma|vlhkost[a-z0-9]*|latex[a-z0-9]*|hrack[a-z0-9]*|halenk[a-z0-9]*|legin[a-z0-9]*|kosil[a-z0-9]*|susenk[a-z0-9]*|knedlik[a-z0-9]*|kapsick[a-z0-9]*|presnid[a-z0-9]*|svacink[a-z0-9]*|povidl[a-z0-9]*|snack[a-z0-9]*|krekry|takis|fuego)\M';
+  veg := s ~ '\m(brambor[a-z0-9]*|cibul[a-z0-9]*|cesnek[a-z0-9]*|rajcat[a-z0-9]*|paprik[a-z0-9]*|okurk[a-z0-9]*|mrkev|petrzel|celer[a-z0-9]*|kedlub[a-z0-9]*|kvetak[a-z0-9]*|brokolic[a-z0-9]*|cuket[a-z0-9]*|lilek|redkv[a-z0-9]*|repa|kapust[a-z0-9]*|zeli|salat[a-z0-9]*|spenat[a-z0-9]*|hras[a-z0-9]*|kukuric[a-z0-9]*|fazol[a-z0-9]*)\M'
+    and s !~ '\m(napoj[a-z0-9]*|dzus[a-z0-9]*|stava|smoothie|pomazank[a-z0-9]*|protlak[a-z0-9]*|pyre|omack[a-z0-9]*|chips[a-z0-9]*|syrov[a-z0-9]*|syr[a-z0-9]*|tunak[a-z0-9]*|lahudk[a-z0-9]*|hotov[a-z0-9]*)\M';
+
+  if beer then tags:=array_append(tags,'beer'); end if;
+  if milk then tags:=array_append(tags,'milk'); end if;
+  if bread then tags:=array_append(tags,'bread'); end if;
+  if s ~ '\mrohlik[a-z0-9]*\M' then tags:=array_append(tags,'rolls'); end if;
+  if s ~ '\mchleb[a-z0-9]*\M' then tags:=array_append(tags,'loaf'); end if;
+  if s ~ '\mbaget[a-z0-9]*\M' then tags:=array_append(tags,'baguette'); end if;
+  if eggs then tags:=array_append(tags,'eggs'); end if;
+  if butter then tags:=array_append(tags,'butter'); end if;
+  if cheese then tags:=array_append(tags,'cheese'); end if;
+  if s ~ '\meidam[a-z0-9]*\M' and not bread then tags:=array_append(tags,'eidam'); end if;
+  if s ~ '\mgouda\M' and not bread then tags:=array_append(tags,'gouda'); end if;
+  if meat then tags:=array_append(tags,'meat'); end if;
+  if chicken then tags:=array_append(tags,'chicken'); end if;
+  if not pet_context and not meat_alternative and s ~ '\mkrkovic[a-z0-9]*\M' then tags:=array_append(tags,'pork_neck'); end if;
+  if not pet_context and not meat_alternative and s ~ '\m(vepr[a-z0-9]*|krkovic[a-z0-9]*|kyta|plec|kotlet[a-z0-9]*|panenk[a-z0-9]*|bucek|koleno)\M' then tags:=array_append(tags,'pork'); end if;
+  if not pet_context and not meat_alternative and s ~ '\m(hovez[a-z0-9]*|svickov[a-z0-9]*|rosten[a-z0-9]*|steak[a-z0-9]*|gulasov[a-z0-9]*)\M' then tags:=array_append(tags,'beef'); end if;
+  if fruit then tags:=array_append(tags,'fruit_fresh'); end if;
+  if s ~ '\mjablk[a-z0-9]*\M' then tags:=array_append(tags,'apples'); end if;
+  if s ~ '\mbanan[a-z0-9]*\M' then tags:=array_append(tags,'bananas'); end if;
+  if fruit_any and s ~ '\mmrazen[a-z0-9]*\M' then tags:=array_append(tags,'fruit_frozen'); end if;
+  if fruit_any and s ~ '\msusen[a-z0-9]*\M' then tags:=array_append(tags,'fruit_dried'); end if;
+  if s ~ '\m(ovocn[a-z0-9]*|jablk[a-z0-9]*|pomeranc[a-z0-9]*|mango|ananas[a-z0-9]*)\M'
+     and s ~ '\m(napoj[a-z0-9]*|dzus[a-z0-9]*|nektar|stava|smoothie)\M' then tags:=array_append(tags,'fruit_drink'); end if;
+  if veg then tags:=array_append(tags,'veg_fresh'); end if;
+  if s ~ '\mbrambor[a-z0-9]*\M' then tags:=array_append(tags,'potatoes'); end if;
+  if s ~ '\mrajcat[a-z0-9]*\M' then tags:=array_append(tags,'tomatoes'); end if;
+  if veg and s ~ '\mmrazen[a-z0-9]*\M' then tags:=array_append(tags,'veg_frozen'); end if;
+  if s ~ '\m(steril[a-z0-9]*|naklad[a-z0-9]*|konzerv[a-z0-9]*)\M'
+     and s ~ '\m(zelenin[a-z0-9]*|okurk[a-z0-9]*|paprik[a-z0-9]*|hras[a-z0-9]*|kukuric[a-z0-9]*|fazol[a-z0-9]*)\M' then tags:=array_append(tags,'veg_preserved'); end if;
+
+  return array(select distinct x from unnest(tags) x order by x);
+end;
+$function$;
